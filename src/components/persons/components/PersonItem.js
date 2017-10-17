e3ae092390d1166520d8a5d534c4137566b74e0a
Andrii Kit <andrii.labyk@gmail.com>
2017-10-17 22:03:47 +0300

    111111

---
+++ src/components/persons/components/PersonItem.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const PersonItem = ({ image, name, dateOfBirth, zodiacSign, titlesNumber }) => (
+    <div className="person-item">
+        <div className="row">
+            <div className="col m-4">
+                <div className="col-inner">
+                    <figure>
+                        <img src={image.url} alt={name.ukr}/>
+                    </figure>
+                    <div className="btn-group">
+                        <button>
+                            <i className="fa fa-heart" aria-hidden="true" />
+                            <span>Додати до улюблених</span>
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <div className="col m-8">
+                <div className="col-inner">
+                    <h2>{name.ukr}</h2>
+                    <p>{name.en}</p>
+                    <p>{dateOfBirth}</p>
+                    <p>{zodiacSign}</p>
+                    <p>{titlesNumber}</p>
+                </div>
+            </div>
+        </div>
+    </div>
+);
+
+export default PersonItem;
