11702c3b065985cb2cb3a1690171f1f09b590bb1
andriy.labyk <andriy.labyk@thinkmobiles.com>
2017-11-03 10:50:06 +0300

    likes

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -7,6 +7,7 @@ import PostsPage from 'Screens/Posts';
 import PostPage from 'Screens/Post';
 import TitlesPage from 'Screens/Titles';
 import PersonsPage from 'Screens/Persons';
+import ReviewsPage from 'Screens/Reviews';
 
 const Routes = () => {
     return (
@@ -20,6 +21,7 @@ const Routes = () => {
                     <Route path='/posts' component={PostsPage} />
                     <Route path='/titles' component={TitlesPage} />
                     <Route path='/persons' component={PersonsPage} />
+                    <Route path='/reviews' component={ReviewsPage} />
                 </Switch>
             </main>
         </div>
---
+++ src/screens/Reviews.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+
+class ReviewsPage extends Component {
+
+    render() {
+        return (
+            <div>Reviews Page</div>
+        );
+    }
+}
+
+export default connect(null, null)(ReviewsPage);
